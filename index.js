c6ec20ad08f63e1850dcff243c2a8f49b70d7033
BadtzMaru08 <ggahelle@gmail.com>
2025-03-01 17:41:11 -0500

    yay4

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function getRandomDog(){
         document.querySelector("#tennis-ball").src = data.message;
     });
 }
+
+function getDog(){
+    let breed = document.querySelector("#breed").value;
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then((res) => {
+        if (!res.ok){
+            //error has occured; we should do somthing!!
+            alert("Bone not found - try again!");
+            return;
+        }
+        console.log(res)
+        return res.json()
+    }).then((data) => {
+        console.log(data);
+        document.querySelector("#tennis-ball").src = data.message;
+    });
+}
+
+
